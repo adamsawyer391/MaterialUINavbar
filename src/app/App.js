946b29650ed50af189552ea2536e395dbaf8d39d
AboveCosmic333 <39273302+adamsawyer391@users.noreply.github.com>
2021-07-14 18:00:21 -0400

    first commit

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+
+import MiniDrawer from '../components/drawer';
+import NavigationDrawer from '../components/drawer';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <NavigationDrawer /> */}
+      <MiniDrawer />
+    </div>
+  );
+}
+
+export default App;
